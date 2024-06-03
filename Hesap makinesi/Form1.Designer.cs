6118af29effcf13aa95740f2ff791aa2615a488e
GÖKHAN <GKHAN@DESKTOP-14PPJLP>
2024-06-03 19:48:43 +0300

    Proje dosyası ekle.

---
+++ Hesap makinesi/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Hesap_makinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ekranLabel = new Label();
+            rakam1Button = new Button();
+            rakam2Button = new Button();
+            rakam3Button = new Button();
+            rakam6Button = new Button();
+            rakam5Button = new Button();
+            rakam4Button = new Button();
+            esittirButton = new Button();
+            rakam0Button = new Button();
+            temizleButton = new Button();
+            rakam9Buttton = new Button();
+            rakam8Button = new Button();
+            rakam7Button = new Button();
+            boluButton = new Button();
+            carpiButton = new Button();
+            eksiButton = new Button();
+            artiButton = new Button();
+            SuspendLayout();
+            // 
+            // ekranLabel
+            // 
+            ekranLabel.BackColor = Color.White;
+            ekranLabel.BorderStyle = BorderStyle.FixedSingle;
+            ekranLabel.Font = new Font("Segoe UI", 40F);
+            ekranLabel.Location = new Point(12, 9);
+            ekranLabel.Name = "ekranLabel";
+            ekranLabel.Size = new Size(394, 95);
+            ekranLabel.TabIndex = 0;
+            ekranLabel.Text = "0";
+            ekranLabel.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // rakam1Button
+            // 
+            rakam1Button.BackColor = Color.PaleTurquoise;
+            rakam1Button.Font = new Font("Segoe UI", 40F);
+            rakam1Button.Location = new Point(12, 118);
+            rakam1Button.Name = "rakam1Button";
+            rakam1Button.Size = new Size(94, 103);
+            rakam1Button.TabIndex = 1;
+            rakam1Button.Text = "1";
+            rakam1Button.UseVisualStyleBackColor = false;
+            rakam1Button.Click += rakam1Button_Click;
+            // 
+            // rakam2Button
+            // 
+            rakam2Button.BackColor = Color.PaleTurquoise;
+            rakam2Button.Font = new Font("Segoe UI", 40F);
+            rakam2Button.Location = new Point(112, 118);
+            rakam2Button.Name = "rakam2Button";
+            rakam2Button.Size = new Size(94, 103);
+            rakam2Button.TabIndex = 2;
+            rakam2Button.Text = "2";
+            rakam2Button.UseVisualStyleBackColor = false;
+            rakam2Button.Click += rakam2Button_Click;
+            // 
+            // rakam3Button
+            // 
+            rakam3Button.BackColor = Color.PaleTurquoise;
+            rakam3Button.Font = new Font("Segoe UI", 40F);
+            rakam3Button.Location = new Point(212, 118);
+            rakam3Button.Name = "rakam3Button";
+            rakam3Button.Size = new Size(94, 103);
+            rakam3Button.TabIndex = 3;
+            rakam3Button.Text = "3 ";
+            rakam3Button.UseVisualStyleBackColor = false;
+            rakam3Button.Click += rakam3Button_Click;
+            // 
+            // rakam6Button
+            // 
+            rakam6Button.BackColor = Color.PaleTurquoise;
+            rakam6Button.Font = new Font("Segoe UI", 40F);
+            rakam6Button.Location = new Point(212, 227);
+            rakam6Button.Name = "rakam6Button";
+            rakam6Button.Size = new Size(94, 103);
+            rakam6Button.TabIndex = 6;
+            rakam6Button.Text = "6";
+            rakam6Button.UseVisualStyleBackColor = false;
+            rakam6Button.Click += rakam6Button_Click;
+            // 
+            // rakam5Button
+            // 
+            rakam5Button.BackColor = Color.PaleTurquoise;
+            rakam5Button.Font = new Font("Segoe UI", 40F);
+            rakam5Button.Location = new Point(112, 227);
+            rakam5Button.Name = "rakam5Button";
+            rakam5Button.Size = new Size(94, 103);
+            rakam5Button.TabIndex = 5;
+            rakam5Button.Text = "5";
+            rakam5Button.UseVisualStyleBackColor = false;
+            rakam5Button.Click += rakam5Button_Click;
+            // 
+            // rakam4Button
+            // 
+            rakam4Button.BackColor = Color.PaleTurquoise;
+            rakam4Button.Font = new Font("Segoe UI", 40F);
+            rakam4Button.Location = new Point(12, 227);
+            rakam4Button.Name = "rakam4Button";
+            rakam4Button.Size = new Size(94, 103);
+            rakam4Button.TabIndex = 4;
+            rakam4Button.Text = "4";
+            rakam4Button.UseVisualStyleBackColor = false;
+            rakam4Button.Click += rakam4Button_Click;
+            // 
+            // esittirButton
+            // 
+            esittirButton.BackColor = Color.Bisque;
+            esittirButton.Font = new Font("Segoe UI", 40F);
+            esittirButton.Location = new Point(212, 445);
+            esittirButton.Name = "esittirButton";
+            esittirButton.Size = new Size(94, 103);
+            esittirButton.TabIndex = 12;
+            esittirButton.Text = "=";
+            esittirButton.UseVisualStyleBackColor = false;
+            esittirButton.Click += esittirButton_Click;
+            // 
+            // rakam0Button
+            // 
+            rakam0Button.BackColor = Color.PaleTurquoise;
+            rakam0Button.Font = new Font("Segoe UI", 40F);
+            rakam0Button.Location = new Point(112, 445);
+            rakam0Button.Name = "rakam0Button";
+            rakam0Button.Size = new Size(94, 103);
+            rakam0Button.TabIndex = 11;
+            rakam0Button.Text = "0";
+            rakam0Button.UseVisualStyleBackColor = false;
+            rakam0Button.Click += rakam0Button_Click;
+            // 
+            // temizleButton
+            // 
+            temizleButton.BackColor = Color.Red;
+            temizleButton.Font = new Font("Segoe UI", 40F);
+            temizleButton.ForeColor = Color.White;
+            temizleButton.Location = new Point(12, 445);
+            temizleButton.Name = "temizleButton";
+            temizleButton.Size = new Size(94, 103);
+            temizleButton.TabIndex = 10;
+            temizleButton.Text = "C";
+            temizleButton.UseVisualStyleBackColor = false;
+            temizleButton.Click += temizleButton_Click;
+            // 
+            // rakam9Buttton
+            // 
+            rakam9Buttton.BackColor = Color.PaleTurquoise;
+            rakam9Buttton.Font = new Font("Segoe UI", 40F);
+            rakam9Buttton.Location = new Point(212, 336);
+            rakam9Buttton.Name = "rakam9Buttton";
+            rakam9Buttton.Size = new Size(94, 103);
+            rakam9Buttton.TabIndex = 9;
+            rakam9Buttton.Text = "9";
+            rakam9Buttton.UseVisualStyleBackColor = false;
+            rakam9Buttton.Click += rakam9Buttton_Click;
+            // 
+            // rakam8Button
+            // 
+            rakam8Button.BackColor = Color.PaleTurquoise;
+            rakam8Button.Font = new Font("Segoe UI", 40F);
+            rakam8Button.Location = new Point(112, 336);
+            rakam8Button.Name = "rakam8Button";
+            rakam8Button.Size = new Size(94, 103);
+            rakam8Button.TabIndex = 8;
+            rakam8Button.Text = "8";
+            rakam8Button.UseVisualStyleBackColor = false;
+            rakam8Button.Click += rakam8Button_Click;
+            // 
+            // rakam7Button
+            // 
+            rakam7Button.BackColor = Color.PaleTurquoise;
+            rakam7Button.Font = new Font("Segoe UI", 40F);
+            rakam7Button.Location = new Point(12, 336);
+            rakam7Button.Name = "rakam7Button";
+            rakam7Button.Size = new Size(94, 103);
+            rakam7Button.TabIndex = 7;
+            rakam7Button.Text = "7";
+            rakam7Button.UseVisualStyleBackColor = false;
+            rakam7Button.Click += rakam7Button_Click;
+            // 
+            // boluButton
+            // 
+            boluButton.BackColor = Color.Pink;
+            boluButton.Font = new Font("Segoe UI", 40F);
+            boluButton.Location = new Point(312, 445);
+            boluButton.Name = "boluButton";
+            boluButton.Size = new Size(94, 103);
+            boluButton.TabIndex = 16;
+            boluButton.Text = "/";
+            boluButton.UseVisualStyleBackColor = false;
+            boluButton.Click += boluButton_Click;
+            // 
+            // carpiButton
+            // 
+            carpiButton.BackColor = Color.Pink;
+            carpiButton.Font = new Font("Segoe UI", 40F);
+            carpiButton.Location = new Point(312, 336);
+            carpiButton.Name = "carpiButton";
+            carpiButton.Size = new Size(94, 103);
+            carpiButton.TabIndex = 15;
+            carpiButton.Text = "x ";
+            carpiButton.UseVisualStyleBackColor = false;
+            carpiButton.Click += carpiButton_Click;
+            // 
+            // eksiButton
+            // 
+            eksiButton.BackColor = Color.Pink;
+            eksiButton.Font = new Font("Segoe UI", 40F);
+            eksiButton.Location = new Point(312, 227);
+            eksiButton.Name = "eksiButton";
+            eksiButton.Size = new Size(94, 103);
+            eksiButton.TabIndex = 14;
+            eksiButton.Text = "-";
+            eksiButton.UseVisualStyleBackColor = false;
+            eksiButton.Click += eksiButton_Click;
+            // 
+            // artiButton
+            // 
+            artiButton.BackColor = Color.Pink;
+            artiButton.Font = new Font("Segoe UI", 40F);
+            artiButton.Location = new Point(312, 118);
+            artiButton.Name = "artiButton";
+            artiButton.Size = new Size(94, 103);
+            artiButton.TabIndex = 13;
+            artiButton.Text = "+";
+            artiButton.UseVisualStyleBackColor = false;
+            artiButton.Click += artiButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(455, 557);
+            Controls.Add(boluButton);
+            Controls.Add(carpiButton);
+            Controls.Add(eksiButton);
+            Controls.Add(artiButton);
+            Controls.Add(esittirButton);
+            Controls.Add(rakam0Button);
+            Controls.Add(temizleButton);
+            Controls.Add(rakam9Buttton);
+            Controls.Add(rakam8Button);
+            Controls.Add(rakam7Button);
+            Controls.Add(rakam6Button);
+            Controls.Add(rakam5Button);
+            Controls.Add(rakam4Button);
+            Controls.Add(rakam3Button);
+            Controls.Add(rakam2Button);
+            Controls.Add(rakam1Button);
+            Controls.Add(ekranLabel);
+            Font = new Font("Segoe UI", 14F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(5);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "HESAP MAKİNESİ ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label ekranLabel;
+        private Button rakam1Button;
+        private Button rakam2Button;
+        private Button rakam3Button;
+        private Button rakam6Button;
+        private Button rakam5Button;
+        private Button rakam4Button;
+        private Button esittirButton;
+        private Button rakam0Button;
+        private Button temizleButton;
+        private Button rakam9Buttton;
+        private Button rakam8Button;
+        private Button rakam7Button;
+        private Button boluButton;
+        private Button carpiButton;
+        private Button eksiButton;
+        private Button artiButton;
+    }
+}
